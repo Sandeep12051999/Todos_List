235eca302ca98423fb1ca627305d32af16cd9b4a
Sandeep12051999 <sandeeppatidar1231@gmail.com>
2022-09-26 12:35:28 +0530

    Sandeep

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an About  Component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quos harum ipsa, vero laudantium animi neque doloribus natus eveniet. Modi odio quas minus adipisci fuga similique nemo reprehenderit? At deserunt consectetur, maxime, necessitatibus quae ut laudantium natus ullam aliquam voluptatem harum.</p>
+        </div>
+  )
+}
